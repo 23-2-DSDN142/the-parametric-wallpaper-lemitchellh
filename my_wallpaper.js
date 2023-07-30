0d6ec78e2fe2e07780a3c333a268fe2327602083
lemitchellh <132876848+lemitchellh@users.noreply.github.com>
2023-07-31 09:13:00 +1200

    Updated wallpaper

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,79 +1,74 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
 
+rayx (120);
+rayy (110);
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 100;
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(0); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  strokeWeight (4);
 
-  fill(orange);
-ellipse (104, 108, 182, 170);
+  stroke (255); 
 
-  fill (255); ///eyeoutline
-  stroke (eyescolor);
-  strokeWeight (eyestroke);
-  rect(righteyeposx, righteyeposy, 40, 40, 35, 35, 5, 5)
-  rect(lefteyeposx, lefteyeposy, 40, 40, 35, 35, 5, 5)
+
+
+  line (100,0,100, 200); //top down
+  //line (130, 0, 30, 200); //top right to left
+  //line (70, 0, 170, 200);
+  line (0, 150, 200, 150);
+  line (0, 50, 200, 50);
+  line (100, 0, 0, 50);
+  line (100, 0, 200, 50);
+  line (100, 200, 0, 150);
+  line (100, 200, 200, 150);
   
-  strokeWeight(0);  ///eyepupils
-  fill (eyecolor)
-  rect(lefteyeposx + 10, lefteyeposy + 7, 20, 30, 35, 35, 5, 5)
-  rect(righteyeposx + 10, righteyeposy + 7, 20, 30, 35, 35, 5, 5)
+  line (1, 1, 100, 100);
+  line (1, 34, 100, 100);
+  line (1, 67, 100, 100);
+  line (1, 100, 100, 100);
+  line (1, 133, 100, 100);
+  line (1, 166, 100, 100);
+  line (1, 200, 100, 100);
 
-  stroke(0)
-  strokeWeight(whiskstroke);
-line (lws, lwsy, lwe, lwey)
-line (lws, lwsy + 3, lwe, lwey + 20)
-line (lws, lwsy + 5, lwe, lwey + 35)
-line (lws, lwsy + 7, lwe, lwey + 50)
-line( rws, lwsy, rwe, lwey);
-line( rws, lwsy + 3, rwe, lwey + 20)
-line( rws, lwsy + 5, rwe, lwey + 35)
-line( rws, lwsy + 7, rwe, lwey + 50)
+  stroke (255, 0, 0)
+line (100, 100, 200, 1);
 
-fill(orange)
-beginShape();
-vertex (40, 43);
-  vertex (30, 15);
-  vertex (55, 35)
-endShape();
+stroke ( 250, 100, 50);
+line (100, 100, 200, 34);
 
-beginShape();
-vertex (159, 38);
-  vertex (184, 13);
-  vertex (174, 53)
-endShape();
+stroke (255, 255, 0);
+line (100, 100, 200, 67);
 
-strokeWeight(nosestroke);
-fill (nosecolor)
-  arc(105, 105, 30, 25, 0, PI, PIE); 
-
-
+stroke(0, 250, 0);
+line (120, 100, 200, 100);
+stroke (0, 0, 250);
+line (100, 100, 200, 133);
+stroke (250, 0, 250);
+line (100, 100, 200, 166);
+stroke (150, 0, 150);
+line (100, 100, 200, 200);
+  stroke(232,244,248);
   
- fill ("#8c0710")
-  rect(60, 145, 90, 40, 9, 9, 30, 30)
 
-  stroke (0);
-  fill ("#eff0cc");
-triangle (85, 145, 95, 165 , 105, 145); 
-triangle (106, 145, 116, 165, 126, 145);  
 
-fill ("#eb3b46");
-arc(105, 185, 50, 20,  PI, TWO_PI, PIE); 
-
+  fill(0);
+  triangle (50, 150, 100, 50, 150, 150);
+  triangle (100, 50, 100, 1, 1, 50);
+ triangle (100, 50, 200, 50, 100, 1);
+ triangle (100, 200, 1, 150, 200, 150);
+  line (100, 1, 100, 200); 
 }
+
